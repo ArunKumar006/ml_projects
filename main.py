603d6c8725d37101ba658f2b1a95fce00482a1e4
arun <arunyuvi0206@gmail.com>
2024-02-16 22:10:18 +0530

    update

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("data.csv")
 x=data[["Hours_of_Exercise"]]
 y=data["Weight"]
-
+print(x,"lll")
 model = LinearRegression()
 model.fit(x,y)
 predictions = model.predict(x)
 print(predictions)
 mse = mean_squared_error(y, predictions)
-plt.scatter(x, y, color='blue')
-plt.plot(x, predictions, color='red', linewidth=2)
+plt.scatter(x, y, color='green')
+plt.plot(x, predictions, color='blue', linewidth=2)
 plt.xlabel('Hours of Exercise')
 plt.ylabel('Weight')
 plt.show()
