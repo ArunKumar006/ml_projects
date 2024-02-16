317f142000623bff4521c96e6c33d21f1ac8aac4
arun <arunyuvi0206@gmail.com>
2024-02-16 12:51:26 +0530

    addded graph using matplotlib

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
 
 data = pd.read_csv("data.csv")
 x=data[["Hours_of_Exercise"]]
@@ -7,5 +10,11 @@ y=data["Weight"]
 
 model = LinearRegression()
 model.fit(x,y)
-prediction = model.predict(x)
-print(prediction)
+predictions = model.predict(x)
+print(predictions)
+mse = mean_squared_error(y, predictions)
+plt.scatter(x, y, color='blue')
+plt.plot(x, predictions, color='red', linewidth=2)
+plt.xlabel('Hours of Exercise')
+plt.ylabel('Weight')
+plt.show()
